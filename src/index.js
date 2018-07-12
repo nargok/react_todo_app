d400c0b51a11d316f8acab795d948dcfa5077b75
Ryoichi Gokan <nargok@gmail.com>
2018-07-12 12:38:25 +0900

    implement react-redus

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import { render } from 'react-dom';
 import tasksReducer from './reducers/tasks'
-import TodoApp from './components/TodoApp'
-import { createStore } from 'redux';
+import TodoApp from './containers/TodoApp'
 
 // Storeを定義
 const store = createStore(tasksReducer)
 
-const renderApp = (store) => {
-  render(
-    <TodoApp store={store}/>,
-    document.getElementById('root')
-  )
-}
-
-store.subscribe(() => renderApp(store))
-renderApp(store)
+render (
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+)
